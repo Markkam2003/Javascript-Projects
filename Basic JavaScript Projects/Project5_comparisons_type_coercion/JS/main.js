d5678b06182f9230a1641e985b5c6f853463ef26
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2022-01-18 13:34:16 -0800

    commit

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -8,3 +8,27 @@ document.write(-3E310);
 document.write(10 > 2);
 document.write(10 < 2);
 console.log(2 + 2);
+document.write(10 == 10);
+document.write(3 == 11);
+X = 10;
+Y = 10;
+document.write(X === Y);
+X = 82;
+Y = "82";
+document.write(X === Y);
+A = 20;
+B = 30;
+document.write(A === B);
+A = 30;
+A = 35;
+document.write(A === A);
+document.write(5 > 2 && 10 > 4);
+document.write(5 > 10 && 10 > 4);
+document.write(5 > 10 || 10 > 20);
+document.write(5 > 10 || 10 > 20);
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5 > 10);
+}
